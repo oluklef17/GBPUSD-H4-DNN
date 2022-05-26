4b33b6a7fce2008e6d81195aa7c54a9cf89e5152
oluklef17 <89250614+oluklef17@users.noreply.github.com>
2022-05-26 13:35:28 +0100

    Add files via upload

--- gbpusd_app.py
+++ gbpusd_app.py
@@ -33,4 +33,4 @@ def predict(data):
 
 if __name__ == '__main__':
   app.secret_key = '491071'
-  app.run(host='0.0.0.0', port='5000', debug=False)
+  app.run(host='0.0.0.0', port='9999', debug=False)
